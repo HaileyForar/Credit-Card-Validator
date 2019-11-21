40d355db63d9c6d2147333a40d32a2fdc13d0d5e
hailey.forar <hailey.forar@MU-C2-08.mullen.pvt.k12.co.us>
2019-11-20 17:12:15 -0700

    finished program

--- Credit Card Validator/src/CreditCardValidator.java
+++ Credit Card Validator/src/CreditCardValidator.java
@@ -2,58 +2,78 @@ import java.util.*;
 import java.io.*;
 
 public class CreditCardValidator
+{
+	public static void main(String[] args)
 	{
-
-		public static void main(String[] args)
-			{
-//			  Scanner file = new Scanner( new File( "prnumbers.txt.dat" ) );
-//			  int numberOfLines = file.nextInt();      
-//			  file.nextLine();
-//			  for(int i = 0; i < numberOfLines; i++ )
-//			     {
-//			       String input = file.nextLine();
-//			     }
-
-			}
-//Help from previous Codingbats and A+
-		
-		public static void validateCreditCardNumber(String str)
+		Scanner file = null;
+		try
 		{
-			
-			int[] ints = new int[str.length()];
-			for(int i = 0; i < str.length(); i++)
+			int validCount = 0;
+			int invalidCount = 0;
+			file = new Scanner( new File( "numbers.txt" ) );
+			while(file.hasNext())
+			{
+				String input = file.nextLine();
+				boolean isValid = validateCreditCardNumber(input);
+				if(isValid)
 				{
-					ints[i] = Integer.parseInt(str.substring(i, i + 1));
+					validCount++;
 				}
-			
-			for(int i = ints.length - 2; i >= 0; i = i - 2)
+				
+				else
 				{
-					int j = ints[i];
-					j = j * 2;
-					if(j > 9)
-						{
-							j = j % 10 + 1;
-						}
-					
-					ints[i] = j;
+					invalidCount++;
 				}
+			}
 			
-			int sum = 0;
-			
-			for(int k = 0; k < ints.length; k++)
-				{
-					sum += ints[k];
-				}
-			
-			if(sum % 10 == 0)
-				{
-					System.out.println("This number, " + str + " is a legal number.");
-				}
-			
-			else
-				{
-					System.out.println("This number," + str + " is NOT a legal number");
-				}
+			System.out.println("The amount of LEGAL numbers is, " + validCount + " and the amount of ILLEGAL numbers is, " + invalidCount);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println(e);
+			return;
+
+		}
+		finally
+		{
+			if (null != file)
+			{
+				file.close();
+			}
 		}
 
 	}
+
+	//Help from previous Codingbats and A+
+
+	public static boolean validateCreditCardNumber(String str)
+	{
+
+		int[] ints = new int[str.length()];
+		for(int i = 0; i < str.length(); i++)
+		{
+			ints[i] = Integer.parseInt(str.substring(i, i + 1));
+		}
+
+		for(int i = ints.length - 2; i >= 0; i = i - 2)
+		{
+			int j = ints[i];
+			j = j * 2;
+			if(j > 9)
+			{
+				j = j % 10 + 1;
+			}
+
+			ints[i] = j;
+		}
+
+		int sum = 0;
+
+		for(int k = 0; k < ints.length; k++)
+		{
+			sum += ints[k];
+		}
+
+		return sum % 10 == 10;
+	}
+}
